c62dfd8410f08ae30c6e51ce804eab5da8d47422
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2006-08-10 18:13:41 +0000

    initial commit

---
+++ src/java/com/idega/hibernate/IWBundleStarter.java
@@ -0,0 +1,18 @@
+package com.idega.hibernate;
+
+import com.idega.idegaweb.IWBundle;
+import com.idega.idegaweb.IWBundleStartable;
+
+
+public class IWBundleStarter implements IWBundleStartable{
+
+	public void start(IWBundle arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop(IWBundle arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
