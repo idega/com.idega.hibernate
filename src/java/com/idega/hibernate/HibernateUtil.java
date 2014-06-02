31d7ebaaad52649009fba47c1807d774c3215afe
Valdas Žemaitis <valdas@idega.com>
2014-06-02 10:19:18 +0300

    New method to lazy load entity(-ies)

--- src/java/com/idega/hibernate/HibernateUtil.java
+++ src/java/com/idega/hibernate/HibernateUtil.java
@@ -1,16 +1,20 @@
 package com.idega.hibernate;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.hibernate.Hibernate;
+import org.hibernate.Transaction;
 import org.hibernate.proxy.HibernateProxy;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.idega.repository.data.Singleton;
+import com.idega.util.DBUtil;
 
-public class HibernateUtil implements Singleton {
-
-	private HibernateUtil() {}
+@Transactional(readOnly = true)
+public class HibernateUtil extends DBUtil {
 
 	@SuppressWarnings("unchecked")
-	public static <T> T initializeAndUnproxy(T entity) {
+	public <T> T initializeAndUnproxy(T entity) {
 		if (entity == null) {
 			throw new NullPointerException("Entity passed for initialization is null");
 		}
@@ -23,4 +27,33 @@ public class HibernateUtil implements Singleton {
 		return entity;
 	}
 
+	public boolean isInitialized(Object object) {
+		return Hibernate.isInitialized(object);
+	}
+
+	@Override
+	public <T> T lazyLoad(T entity) {
+		org.hibernate.Session s = null;
+		Transaction transaction = null;
+		try {
+			s = SessionFactoryHelper.getInstance().getSessionFactory().openSession();
+			transaction = s.beginTransaction();
+			if (!s.isConnected()) {
+				Logger.getLogger(getClass().getName()).warning("Session is not opened, can not lazy load entity!");
+			}
+			
+			s.refresh(entity);
+			entity = initializeAndUnproxy(entity);
+		} catch (Exception e) {
+			Logger.getLogger(getClass().getName()).log(Level.WARNING, "Error initializing entity " + entity, e);
+		} finally {
+			transaction.commit();
+			if (s.isOpen()) {
+				s.close();
+			}
+		}
+
+		return entity;
+	}
+	
 }
