37684af3cd41abcf1324cf35ed3a2c189855e7d5
Valdas Žemaitis <valdas@idega.com>
2014-10-01 10:30:59 +0300

    Enabling 2nd level Hibernate cache

--- src/java/com/idega/hibernate/HibernateUtil.java
+++ src/java/com/idega/hibernate/HibernateUtil.java
@@ -3,8 +3,11 @@ package com.idega.hibernate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.persistence.Query;
+
 import org.hibernate.Hibernate;
 import org.hibernate.Transaction;
+import org.hibernate.ejb.HibernateQuery;
 import org.hibernate.proxy.HibernateProxy;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +16,9 @@ import com.idega.util.DBUtil;
 @Transactional(readOnly = true)
 public class HibernateUtil extends DBUtil {
 
+	private static final Logger LOGGER = Logger.getLogger(HibernateUtil.class.getName());
+
+	@Override
 	@SuppressWarnings("unchecked")
 	public <T> T initializeAndUnproxy(T entity) {
 		if (entity == null) {
@@ -27,6 +33,7 @@ public class HibernateUtil extends DBUtil {
 		return entity;
 	}
 
+	@Override
 	public boolean isInitialized(Object object) {
 		return Hibernate.isInitialized(object);
 	}
@@ -39,13 +46,13 @@ public class HibernateUtil extends DBUtil {
 			s = SessionFactoryHelper.getInstance().getSessionFactory().openSession();
 			transaction = s.beginTransaction();
 			if (!s.isConnected()) {
-				Logger.getLogger(getClass().getName()).warning("Session is not opened, can not lazy load entity!");
+				LOGGER.warning("Session is not opened, can not lazy load entity!");
 			}
-			
+
 			s.refresh(entity);
 			entity = initializeAndUnproxy(entity);
 		} catch (Exception e) {
-			Logger.getLogger(getClass().getName()).log(Level.WARNING, "Error initializing entity " + entity, e);
+			LOGGER.log(Level.WARNING, "Error initializing entity " + entity, e);
 		} finally {
 			transaction.commit();
 			if (s.isOpen()) {
@@ -55,5 +62,25 @@ public class HibernateUtil extends DBUtil {
 
 		return entity;
 	}
-	
+
+	@Override
+	public void doInitializeCaching(Query query, String cacheRegion) {
+		if (query instanceof org.hibernate.Query) {
+			org.hibernate.Query hQuery = (org.hibernate.Query) query;
+			hQuery.setCacheable(true);
+			if (cacheRegion != null) {
+				hQuery.setCacheRegion(cacheRegion);
+			}
+		} else if (query instanceof HibernateQuery) {
+			HibernateQuery hQuery = (HibernateQuery) query;
+			org.hibernate.Query tmp = hQuery.getHibernateQuery();
+			tmp.setCacheable(true);
+			if (cacheRegion != null) {
+				tmp.setCacheRegion(cacheRegion);
+			}
+		} else {
+			LOGGER.warning("Query is not type of " + org.hibernate.Query.class.getName() + ", can not use caching");
+		}
+	}
+
 }
